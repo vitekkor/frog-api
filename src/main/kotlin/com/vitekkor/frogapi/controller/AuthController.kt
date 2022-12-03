607e9e9b0c1008fae8e0edc590924b9dff9d76a7
vitekkor <viktor.korotkikh@gmail.com>
2022-12-03 22:50:29 +0300

    #2: add user creation handling

--- src/main/kotlin/com/vitekkor/frogapi/controller/AuthController.kt
+++ src/main/kotlin/com/vitekkor/frogapi/controller/AuthController.kt
@@ -1,6 +1,9 @@
 package com.vitekkor.frogapi.controller
 
+import com.vitekkor.frogapi.db.entity.User
+import com.vitekkor.frogapi.db.repository.UserRepository
 import com.vitekkor.frogapi.model.SingUpModel
+import com.vitekkor.frogapi.util.encodeBase64
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -11,7 +14,7 @@ import org.springframework.web.servlet.ModelAndView
 
 @Controller
 @RequestMapping("/auth/singup")
-class AuthController {
+class AuthController(private val userRepository: UserRepository) {
     @GetMapping
     fun singUpPage(model: Model): ModelAndView {
         val modelAndView = ModelAndView()
@@ -21,7 +24,28 @@ class AuthController {
     }
 
     @PostMapping
-    fun singUp(@ModelAttribute singUpModel: SingUpModel): String {
-        return "singUpSuccessful"
+    fun singUp(@ModelAttribute singUpModel: SingUpModel, model: Model): String {
+        if (singUpModel.isValid()) {
+            if (!singUpModel.checkPassword()) {
+                model.addAttribute("singUpModel", singUpModel.copy(badPassword = "bad"))
+                return "singUp"
+            }
+            val alreadyExistsUser = userRepository.findOneByEmail(checkNotNull(singUpModel.email))
+            return if (alreadyExistsUser == null) {
+                val user = User(
+                    name = singUpModel.name,
+                    email = singUpModel.email,
+                    password = singUpModel.password?.encodeBase64()
+                )
+                userRepository.saveAndFlush(user)
+                "singUpSuccessful"
+            } else {
+                model.addAttribute("singUpModel", singUpModel.copy(badEmail = "bad"))
+                "singUp"
+            }
+        } else {
+            model.addAttribute("singUpModel", singUpModel.copy(badEmail = "bad", badPassword = "bad"))
+            return "singUp"
+        }
     }
 }
