d7c931930d11a67eef47dd4470bb6f9ebbb7e746
vitekkor <viktor.korotkikh@gmail.com>
2022-12-03 22:47:27 +0300

    #2: add fields validation infrastructure

--- src/main/kotlin/com/vitekkor/frogapi/model/SingUpModel.kt
+++ src/main/kotlin/com/vitekkor/frogapi/model/SingUpModel.kt
@@ -1,5 +1,15 @@
 package com.vitekkor.frogapi.model
 
-data class SingUpModel(val name: String?, val email: String?, val password: String?) {
-    constructor() : this(null, null, null)
+data class SingUpModel(
+    val name: String? = null,
+    val email: String? = null,
+    val password: String? = null,
+    val confirmPassword: String? = null,
+    val badEmail: String = "",
+    val badPassword: String = ""
+) {
+    fun isValid(): Boolean =
+        !name.isNullOrEmpty() && !email.isNullOrEmpty() && !password.isNullOrEmpty() && !confirmPassword.isNullOrEmpty()
+
+    fun checkPassword(): Boolean = password == confirmPassword
 }
